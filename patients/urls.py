4ef3899c878fafca927447011530ee08330f888f
dcl10 <lea.daniel@hotmail.co.uk>
2017-11-18 17:14:32 +0000

    random updates, don't know what I changed it's been so long.

--- patients/urls.py
+++ patients/urls.py
@@ -5,8 +5,7 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    url(r'^$', login_required(ListView.as_view(queryset=Patient.objects.all(), template_name="patients/patients.html"),
-                              login_url="/")),
+    url(r'^$', views.list_patients),
     url(r'^(?P<pk>\d+)$', login_required(DetailView.as_view(model=Patient, template_name="patients/info.html"),
                                          login_url="/")),
     url(r'^add/', views.add_patient),
--- patients/views.py
+++ patients/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_protect, requires_csrf_token
 from .models import Patient
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+
 
 @login_required(login_url="/")
 def add_patient(request):
     return render(request, "patients/add_patient.html")
+
 
 @login_required(login_url="/")
 def patient_added(request):
@@ -19,7 +23,7 @@ def patient_added(request):
         first_incident = request.POST['#1_incident']
         dead_censor = request.POST['dead_censor']
         snumber = request.POST['snumber']
-        if request.POST['deceased'] is 'True':
+        if request.POST['deceased'] == 'True':
             deceased = True
         else:
             deceased = False
@@ -30,3 +34,20 @@ def patient_added(request):
         return render(request, "patients/patient_added.html", {'patient': str(nhs_number) +' added successfully'})
     except Exception:
         return render(request, "patients/patient_added.html", {'patient': 'Error: patient not added'})
+
+
+@login_required(login_url="/")
+def list_patients(request):
+    patient_list = Patient.objects.all()
+    paginator = Paginator(patient_list, 15)
+    page = request.GET.get('page')
+    try:
+        patients = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        patients = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        patients = paginator.page(paginator.num_pages)
+
+    return render(request, 'patients/patients.html', {'patients': patients})
